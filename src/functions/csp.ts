84598059cb08fa384576ce56c2565a907639cfaf
arvinxx <arvinx@foxmail.com>
2021-01-13 04:53:19 +0800

    :bug: fix: 修复 build 时 CSP 报错的问题

--- src/functions/csp.ts
+++ src/functions/csp.ts
@@ -3,7 +3,12 @@ import { join } from 'path';
 import { uniq } from 'lodash';
 
 import fse from 'fs-extra';
-import { CSPKeyMap, getCSPHashFromScript, updateCSP } from '../utils';
+import {
+  CSPKeyMap,
+  extractInlineScript,
+  getScriptSHA,
+  UIPageKeyMap,
+} from '../utils';
 
 /**
  * 处理 Chrome 的内容安全政策(CSP)相关的问题
@@ -16,7 +21,6 @@ import { CSPKeyMap, getCSPHashFromScript, updateCSP } from '../utils';
  */
 export default (api: IApi) => {
   const { paths } = api.service;
-  const filepath = join(paths.absOutputPath!, 'manifest.json');
 
   // 在配置项中注入该钩子字符串
   api.modifyConfig((config) => {
@@ -30,18 +34,43 @@ export default (api: IApi) => {
   /**
    * 写入 inline script 的 hash
    */
-  const writeInlineScriptHash = async () => {
-    await getCSPHashFromScript(paths.absOutputPath);
+  const done = () => {
+    // 分别处理 popup 和 options 的 html 文件
+    const { option, popup } = UIPageKeyMap;
+    const htmlPaths: string[] = [];
+
+    if (option.output !== '') {
+      htmlPaths.push(join(paths.absOutputPath!, option.output));
+    }
+    if (popup.output !== '') {
+      htmlPaths.push(join(paths.absOutputPath!, popup.output));
+    }
+
+    const scriptList: string[] = [];
+
+    htmlPaths.forEach((htmlPath) => {
+      const html = fse.readFileSync(htmlPath, { encoding: 'utf-8' });
+
+      // 将 html 中的 inlineScript 全部记录到 inlineScriptSHAList 中
+      extractInlineScript(html).forEach((script) => {
+        scriptList.push(getScriptSHA(script));
+      });
+    });
+
+    // 待写入的 hash 字符串
+    const scriptStr = uniq<string>(scriptList)
+      .map((s) => `sha256-${s}`)
+      .join("' '");
 
     // 将 script 写入 manifest 中
-    const manifest = fse.readFileSync(filepath, { encoding: 'utf8' });
-
-    fse.writeFileSync(filepath, updateCSP(manifest));
+    const manifestFile = join(paths.absOutputPath!, 'manifest.json');
+    let manifest = fse.readFileSync(manifestFile, { encoding: 'utf8' });
+    manifest = manifest.replace(CSPKeyMap.key, scriptStr);
+    fse.writeFileSync(manifestFile, manifest);
   };
 
   // Dev 下需要
-  api.onDevCompileDone(writeInlineScriptHash);
-
   // build 下也需要
-  api.onBuildComplete(writeInlineScriptHash);
+  api.onDevCompileDone(done);
+  api.onBuildComplete(done);
 };
